49a39d36c1cd0c21b6f8e51414a0e164bf99dde8
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-07-10 03:07:12 +0400

    Ktor API Service basic setup for Android application

--- androidApp/src/main/java/ru/fursa/unsplash/android/MainActivity.kt
+++ androidApp/src/main/java/ru/fursa/unsplash/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.fursa.unsplash.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -8,15 +9,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ru.fursa.unsplash.Greeting
+import ru.fursa.unsplash.UnsplashKtorClient
 
 @Composable
 fun MyApplicationTheme(
@@ -58,6 +59,7 @@ fun MyApplicationTheme(
 }
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -66,22 +68,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting(Greeting().greeting())
+
                 }
             }
+
+            LaunchedEffect(key1 = Unit, block = {
+                val client = UnsplashKtorClient()
+                client.getCollections().forEach { it ->
+                    println("!!!! $it")
+                }
+            })
         }
     }
 }
 
-@Composable
-fun Greeting(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Hello, Android!")
-    }
-}
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
+    kotlin("plugin.serialization") version("1.5.20")
 }
 
 kotlin {
@@ -22,13 +23,31 @@ kotlin {
     }
     
     sourceSets {
-        val commonMain by getting
+
+        val ktorVersion = "2.3.2"
+
+        val commonMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+                implementation("io.ktor:ktor-client-logging:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+            }
+        }
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -38,6 +57,7 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+        //    implementation("io.ktor:ktor-client-darwin:$ktorVersion")
         }
         val iosX64Test by getting
         val iosArm64Test by getting
