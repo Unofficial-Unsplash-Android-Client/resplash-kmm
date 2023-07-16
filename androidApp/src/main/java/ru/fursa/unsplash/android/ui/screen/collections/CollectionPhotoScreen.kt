f91816bd9673ae457b266ad1ce36809d67435f4b
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-07-17 01:50:24 +0400

    Tabs navigation implemented

--- androidApp/src/main/java/ru/fursa/unsplash/android/ui/screen/collections/CollectionPhotoScreen.kt
+++ androidApp/src/main/java/ru/fursa/unsplash/android/ui/screen/collections/CollectionPhotoScreen.kt
@@ -1,18 +1,27 @@
 package ru.fursa.unsplash.android.ui.screen.collections
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.UiComposable
 import androidx.compose.ui.unit.dp
 
-@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
+@UiComposable
 fun CollectionPhotoScreen() {
-    Column(modifier = Modifier.fillMaxSize().padding(top = 60.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = "Collection photo screen")
     }
 }
--- androidApp/src/main/java/ru/fursa/unsplash/android/ui/tabs/TabScreen.kt
+++ androidApp/src/main/java/ru/fursa/unsplash/android/ui/tabs/TabScreen.kt
@@ -1,80 +1,64 @@
 package ru.fursa.unsplash.android.ui.tabs
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.ScrollableTabRow
-import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import ru.fursa.unsplash.android.ui.screen.collections.CollectionPhotoScreen
-import ru.fursa.unsplash.android.ui.screen.single.SinglePhotoScreen
+import ru.fursa.unsplash.android.R
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun TabScreen() {
-    var selectedTabIndex by remember { mutableStateOf(0) }
-    val tabs = listOf("Single", "Collections")
-    val scrollableTabIndex = remember { mutableStateOf(selectedTabIndex) }
+fun TabScreen(
+    onTabIndexChanged: @Composable (index: Int) -> Unit
+) {
+    val tabs = listOf(
+        stringResource(id = R.string.tab_item_single),
+        stringResource(id = R.string.tab_item_collections)
+    )
+
+    val scrollableTabIndex = remember { mutableStateOf(0) }
 
     Column(modifier = Modifier.fillMaxWidth()) {
-        val pagerState = rememberPagerState(initialPage = selectedTabIndex)
+        val pagerState = rememberPagerState(initialPage = 0)
 
-        ScrollableTabRow(
-            selectedTabIndex = selectedTabIndex,
+        TabRow(selectedTabIndex = scrollableTabIndex.value,
             modifier = Modifier.height(48.dp),
-            edgePadding = 16.dp,
             indicator = { positions ->
                 TabRowDefaults.Indicator(
-                    modifier = Modifier
-                        .tabIndicatorOffset(positions[scrollableTabIndex.value])
+                    modifier = Modifier.tabIndicatorOffset(positions[scrollableTabIndex.value])
                 )
             }) {
 
             tabs.forEachIndexed { index, title ->
-                Tab(
-                    selected = scrollableTabIndex.value == index,
-                    onClick = {
-                        scrollableTabIndex.value = index
-                        selectedTabIndex = index
-                    },
-                    text = { Text(text = title) },
-                    modifier = Modifier.weight(1f).fillMaxHeight()
+                TabItem(
+                    selectedTabIndex = pagerState.currentPage,
+                    tabIndex = index,
+                    tabTitle = title,
+                    pagerState = pagerState
                 )
             }
 
         }
 
         HorizontalPager(
-            pageCount = tabs.size,
-            state = pagerState,
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            when (it) {
-                0 -> SinglePhotoScreen()
-                1 -> CollectionPhotoScreen()
-            }
-        }
+            pageCount = tabs.size, state = pagerState, modifier = Modifier.fillMaxWidth()
+        ) { pageIndex -> onTabIndexChanged(pageIndex) }
 
-        LaunchedEffect(key1 = pagerState.currentPage, block = {
+        LaunchedEffect(key1 = pagerState.currentPage) {
             scrollableTabIndex.value = pagerState.currentPage
-        })
+        }
 
     }
 }
-
