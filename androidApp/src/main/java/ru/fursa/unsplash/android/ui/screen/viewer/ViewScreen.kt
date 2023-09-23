081e0bd1f9ceb74d6c14e1c381bcc59ca7d6ebca
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-09-23 16:47:53 +0400

    Added preloading animation to viewer screen

--- androidApp/src/main/java/ru/fursa/unsplash/android/ui/screen/viewer/ViewScreen.kt
+++ androidApp/src/main/java/ru/fursa/unsplash/android/ui/screen/viewer/ViewScreen.kt
@@ -1,14 +1,19 @@
 package ru.fursa.unsplash.android.ui.screen.viewer
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.compose.LocalImageLoader
@@ -16,6 +21,7 @@ import coil.request.ImageRequest
 import org.koin.androidx.compose.koinViewModel
 import ru.fursa.unsplash.android.base.screen.ErrorScreen
 import ru.fursa.unsplash.android.ui.kit.compound.DialogWindow
+import ru.fursa.unsplash.android.ui.kit.compound.DotsPreloader
 
 @Composable
 fun ViewScreen(
@@ -36,16 +42,29 @@ fun ViewScreen(
             when (viewState.value) {
                 ViewerViewModel.ImageLoadState.Loading -> {
                     DialogWindow(content = {
-                        CircularProgressIndicator()
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color.White),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            DotsPreloader(
+                                circleColor = Color.LightGray,
+                                circleSize = 12.dp,
+                                spaceBetween = 15.dp
+                            )
+                        }
                     }) {
                         navController.navigateUp()
                     }
                 }
+
                 is ViewerViewModel.ImageLoadState.ErrorLoading -> {
                     val message = (viewState.value as? ViewerViewModel.ImageLoadState.ErrorLoading)
                         ?.message.orEmpty()
                     ErrorScreen(message)
                 }
+
                 is ViewerViewModel.ImageLoadState.Success -> {
                     AsyncImage(
                         model = ImageRequest.Builder(context)
