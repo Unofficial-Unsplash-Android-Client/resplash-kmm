c46833d2bed73c07d400e0510a3632e9ff280370
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-10-11 02:58:39 +0400

    edit mvi contract for home screen

--- androidApp/src/main/java/ru/fursa/unsplash/android/ui/screen/home/HomeMVIContract.kt
+++ androidApp/src/main/java/ru/fursa/unsplash/android/ui/screen/home/HomeMVIContract.kt
@@ -1,18 +1,12 @@
 package ru.fursa.unsplash.android.ui.screen.home
 
-import androidx.annotation.StringRes
 import ru.fursa.unsplash.android.base.mvi.UiEffect
 import ru.fursa.unsplash.android.base.mvi.UiEvent
 import ru.fursa.unsplash.android.base.mvi.UiState
 import ru.fursa.unsplash.data.ui.models.PhotoModel
 
 class HomeMVIContract {
-    sealed class Event : UiEvent {
-        object Loading : Event()
-        data class Error(val message: String) : Event()
-        data class Success(val data: List<PhotoModel>) : Event()
-    }
-
+    sealed class Event : UiEvent
     data class State(
         val loading: Boolean = false,
         val success: Boolean = false,
@@ -21,8 +15,5 @@ class HomeMVIContract {
         val message: String = "",
         val data: List<PhotoModel> = emptyList()
     ) : UiState
-
-    sealed class Effect : UiEffect {
-        data class ShowToastMessage(@StringRes val message: Int) : Effect()
-    }
+    sealed class Effect : UiEffect
 }
